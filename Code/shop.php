d2a495fe57c5a97b44acd39b103814c4ce3a4c32
renzomancini05 <renzomancini05@gmail.com>
2024-05-19 12:38:27 +0200

    modificato shop.php

--- Code/shop.php
+++ Code/shop.php
@@ -6,53 +6,23 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-  <div class="container text-center">
-  <div class="row align-items-start my-5">
-    <div class="col">
-    <a class="btn btn-secondary btn-lg" href="Carrello.php" role="button">Carrello</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <div class="container text-center">
+      <div class="row align-items-start my-5">
+        <div class="col">      
+          <div class="card" style="width: 18rem;">
+            <img src="img/Hedge.jpg" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title">777.Finance</h5>
+              <p class="card-text">777.finance: un hedge fund innovativo, focalizzato sui rendimenti superiori attraverso strategie di investimento basate sui dati e sull'utilizzo di algoritmi avanzati.</p>
+              <a href="777Finance.php" class="btn btn-secondary">Scopri di più</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="col">
-      
-    <div class="card" style="width: 18  rem;">
-  <img src="img/Hedge.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">777.Finance</h5>
-    <p class="card-text">777.finance: un hedge fund innovativo, focalizzato sui rendimenti superiori attraverso strategie di investimento basate sui dati e sull'utilizzo di algoritmi avanzati.</p>
-    <a href="777Finance.php" class="btn btn-secondary">Scopri di più</a>
-  </div>
-  
-</div>
-
-
-    </div>
-    <div class="col">
- 
-</div>
-    </div>
-    <div class="col">
-    <
-</div>
-
-    </div>
-  </div>
-</div>
-    
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
